586182202c314b3d383d8e5c1af3d6fc7ff0796c
Thomas Wright <thomas.wright@caliqon.com>
2023-11-10 19:45:09 +0000

    updated fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+games { 'rdr3', 'gta5' }
+fx_version 'cerulean'
+
+author 'ItsNemesi'
+description 'Nemesi NPGSQL'
+version '0.1.0'
+
+-- What to run
+client_scripts 'build/client.js'
+server_script 'build/server.js'
